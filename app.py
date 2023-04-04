08f313625fcdaab131fcf37b487d1c0637708e90
SofianB <s.brhaiberh@outlook.fr>
2023-04-04 15:27:09 +0200

    Co-authored-by: KyabeSamaEPSI <KyabeSamaEPSI@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from Prediction import Prediction
 from flask_restful import  Api 
-from routes import 
 
 app = Flask("Konoha_Team7")
+
 api = Api(app)
+
+api.add_resource(Prediction, '/prediction/<prediction_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
--- routes.py
+++
@@ -1,4 +0,0 @@
-from Prediction import Prediction
-from app import api
-
-api.add_resource(Prediction, '/prediction/<prediction_id>')
