b5c5e764f5ba047fef25489dbd12a1f6fc8f3131
KyabeSama <guillaume.ros.pro@gmail.com>
2023-04-05 10:10:51 +0200

    html templating

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from Prediction import Prediction
 from flask_restful import  Api 
 
@@ -6,7 +6,16 @@ app = Flask("Konoha_Team7")
 
 api = Api(app)
 
-api.add_resource(Prediction, '/prediction/<prediction_id>')
+@app.route("/prediction/<prediction_id>")
+def prediction():
+    
+    return Prediction.get()
+
+@app.route("/")
+def home():
+    title = "Konoha_Team7"
+    return render_template("index.html.j2", title=title)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python app.py
+#docker run -d --name=grafana -p 3000:3000 grafana/grafana
