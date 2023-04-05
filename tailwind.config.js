178bc992184e586b6a382efe3dcb4cb514e322dd
SofianB <s.brhaiberh@outlook.fr>
2023-04-05 11:04:02 +0200

    Debut templating

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html.j2",
+    "./static/src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
